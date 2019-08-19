a11327914d8567316eb9604fb882784aed1fd2b1
samuel787 <suther.david.samuel@gmail.com>
2019-08-19 21:26:59 +0800

    Level 2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________\n"
+                        + "    Hello! I'm Duke\n"
+                        + "    What can I do for you?\n"
+                        + "    ____________________________________________________________\n"
+        );
+
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            String user_input = scanner.nextLine();
+            if(user_input.equals("bye")){
+                System.out.println("    ____________________________________________________________\n"
+                        + "    Bye. Hope to see you again soon!\n"
+                        + "    ____________________________________________________________\n"
+                );
+                break; // breaks out of loop and ends Duke
+            } else {
+                System.out.println("    ____________________________________________________________\n"
+                        + "    " + user_input + "\n"
+                        + "    ____________________________________________________________\n"
+                );
+            }
+        }
     }
 }
