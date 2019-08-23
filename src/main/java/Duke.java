3eebcb4dd4465917c362bdd67b975037b497d8bf
samuel787 <suther.david.samuel@gmail.com>
2019-08-24 01:14:32 +0800

    Task 3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,7 +11,7 @@ public class Duke {
 
         duke_start(duke_line, duke_indent);
 
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while(true){
             Scanner scanner = new Scanner(System.in);
@@ -20,10 +21,10 @@ public class Duke {
                 break;
             } else if(user_input.equals("list")){
                 duke_list(duke_line, duke_indent, tasks);
-            }
-            else {
-                tasks.add(user_input);
-                duke_add(duke_line, duke_indent, user_input);
+            } else if(user_input.length() >= 4 && user_input.substring(0,4).equals("done")){
+                duke_done(duke_line, duke_indent, tasks, user_input);
+            } else {
+                duke_add(duke_line, duke_indent, tasks, user_input);
             }
         }
     }
@@ -41,17 +42,37 @@ public class Duke {
         System.out.println(duke_line);
     }
 
-    private static void duke_add(String duke_line, String duke_indent, String task){
+    private static void duke_add(String duke_line, String duke_indent, ArrayList<Task> tasks, String user_input){
+        tasks.add(new Task(user_input));
         System.out.println(duke_line);
-        System.out.println(duke_indent + "added: "+task+"\n");
+        System.out.println(duke_indent + "added: "+user_input+"\n");
         System.out.println(duke_line);
     }
 
-    private static void duke_list(String duke_line, String duke_indent, ArrayList<String> tasks){
+    private static void duke_list(String duke_line, String duke_indent, ArrayList<Task> tasks){
         System.out.println(duke_line);
         for(int i=1; i<= tasks.size(); i++){
             System.out.println(duke_indent + i+". "+tasks.get(i-1)+"\n");
         }
         System.out.println(duke_line);
+    }
+
+    private static void duke_done(String duke_line, String duke_indent, ArrayList<Task> tasks, String user_input){
+        try{
+            int task_num = Integer.parseInt(user_input.substring(5)) - 1;
+            if(task_num >= 0){
+                tasks.get(task_num).setDone();
+                System.out.println(duke_line);
+                System.out.println(duke_indent+"Nice! I've marked this task as done:");
+                System.out.println(duke_indent+tasks.get(task_num));
+                System.out.println(duke_line);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e){
+            duke_add(duke_line, duke_indent, tasks, user_input);
+        } catch (IllegalArgumentException e){
+            duke_add(duke_line, duke_indent, tasks, user_input);
+        }
     }
 }
