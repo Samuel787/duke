0d4c0f07b059fa59b095578645acf6937c2fc880
samuel787 <suther.david.samuel@gmail.com>
2019-09-11 11:39:13 +0800

    Minor changes to add command testing

--- src/main/java/Test/ParserTest.java
+++ src/main/java/Test/ParserTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ParserTest {
-
+    
     //basic necessities
     private final String file_path = "data/dukeTest.txt";
     private Ui ui;
